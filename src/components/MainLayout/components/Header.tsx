c59e7491ec1763257a7158b6e2a6dcc66226a9db
Rostislav Sergeenkov <rostislav.sergeenkov@gmail.com>
2022-10-03 20:45:51 +0200

    Feat: implement automated infrastructure creation, add minor changes to the store, update README.md

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            My CloudX Store: frontend (deployed by Rostislav Sergeenkov)
           </Link>
         </Typography>
 
