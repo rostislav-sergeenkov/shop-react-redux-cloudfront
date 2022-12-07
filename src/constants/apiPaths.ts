4eaa541a6dbeb9f774521c9a0f7c79188a369d1f
Rostislav Sergeenkov <rostislav.sergeenkov@gmail.com>
2022-12-07 01:53:41 +0100

    feat: cart api url was updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://pjnon3ot88.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://ta6mjyl089.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://1kbvtvvvu0.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
